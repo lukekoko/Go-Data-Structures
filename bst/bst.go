3c2c4c6c1246e861bf48b56db6f3e74a70bae474
Luke Koko <lukekoko1@gmail.com>
2020-01-28 15:10:56 +1100

    BST done

--- bst/bst.go
+++ bst/bst.go
@@ -55,25 +55,29 @@ func (tree *bst) search(val int, node *Node) *Node {
 	}
 }
 
-func (tree *bst) delete(val int) {
-	tree.deleteNode(val, tree.root)
+func (tree *bst) delete() {
+	tree.root = nil
 }
 
-func (tree *bst) deleteNode(val int, root *Node) *Node {
+func (tree *bst) remove(val int) {
+	tree.removeNode(val, tree.root)
+}
+
+func (tree *bst) removeNode(val int, root *Node) *Node {
 	if root == nil {
 		return nil
 	}
 	if val < root.data {
-		root.left = tree.deleteNode(val, root.left)
+		root.left = tree.removeNode(val, root.left)
 	} else if val > root.data {
-		root.right = tree.deleteNode(val, root.right)
+		root.right = tree.removeNode(val, root.right)
 	} else {
 		if root.left == nil && root.right == nil {
 			root = nil
 		} else if root.left != nil && root.right != nil {
 			min := tree.min(root.right)
 			root.data = min.data
-			root.right = tree.deleteNode(min.data, root.right)
+			root.right = tree.removeNode(min.data, root.right)
 		} else if root.left == nil {
 			*root = *root.right
 		} else if root.right == nil {
@@ -81,6 +85,18 @@ func (tree *bst) deleteNode(val int, root *Node) *Node {
 		}
 	}
 	return root
+}
+
+func (tree *bst) print(traversal string) {
+	if traversal == "inorder" {
+		tree.inorder(tree.root)
+	}
+	if traversal == "preorder" {
+		tree.preorder(tree.root)
+	}
+	if traversal == "postorder" {
+		tree.postorder(tree.root)
+	}
 }
 
 func (tree *bst) inorder(node *Node) {
@@ -101,11 +117,37 @@ func (tree *bst) preorder(node *Node) {
 	tree.inorder(node.right)
 }
 
+func (tree *bst) postorder(node *Node) {
+	if node == nil {
+		return
+	}
+	tree.inorder(node.left)
+	tree.inorder(node.right)
+	fmt.Println(node.data)
+}
+
 func (tree *bst) min(node *Node) *Node {
 	if node.left == nil {
 		return node
 	}
 	return tree.min(node.left)
+}
+
+func (tree *bst) max(node *Node) *Node {
+	if node.right == nil {
+		return node
+	}
+	return tree.max(node.right)
+}
+
+func (tree *bst) isBST(node *Node, min int, max int) bool {
+	if node == nil {
+		return true
+	}
+	if node.data < min || node.data > max {
+		return false
+	}
+	return tree.isBST(node.left, min, node.data-1) && tree.isBST(node.right, node.data+1, max)
 }
 
 func main() {
@@ -117,12 +159,9 @@ func main() {
 	tree.insert(14)
 	tree.insert(9)
 	tree.insert(8)
-	tree.delete(7)
-	tree.delete(8)
-	tree.delete(9)
-	tree.delete(14)
-	tree.delete(1)
-	tree.delete(3)
-	tree.preorder(tree.root)
-
+	tree.print("inorder")
+	tree.delete()
+	tree.print("postorder")
+	tree.insert(2)
+	
 }
